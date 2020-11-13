14e894ed50febde29cf2ec6d5a9db2e193dbbb32
egeal <egealtiok@gmail.com>
2020-11-13 21:57:00 +0300

    Google Maps integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,15 +8,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(38.423672, 27.142806),
+    zoom: 15,
+  );
+
+  static final CameraPosition _kGoLocation = CameraPosition(
+      target: LatLng(38.388103, 27.044829),
+      zoom: 15);
+  // bearing: 192.8334901395799,
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goTo,
+        label: Text('Go To Location'),
+        icon: Icon(Icons.work),
+      ),
+
+    );
+  }
+
+  Future<void> _goTo() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kGoLocation));
   }
 }
